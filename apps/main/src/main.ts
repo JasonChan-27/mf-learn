fb182b1e7f1b1bd2038d05c003bb6172ad1f8657
亚杰 <13716897712@163.com>
2026-01-29 10:08:33 +0800

    CI测试

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -3,7 +3,7 @@ import { initMetrics } from 'mf-telemetry'
 import './style.css'
 import App from './App.vue'
 
-;(window as any).__MF_METRICS_ENDPOINT__ = 'http://localhost:5000'
+// ;(window as any).__MF_METRICS_ENDPOINT__ = 'http://localhost:5000'
 
 // init metrics if endpoint configured in global
 try {
